6f8999a757a1eac4b9cae159fa6f2b8d32f120d7
YeabsiraNigusse <nigusseyeabsira@gmail.com>
2025-10-03 17:24:21 +0300

    done with guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,43 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main(){
-    println!("guessing the number");
+    println!("guess the number");
 
-    let mut guess = String::new();
+    let number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin().read_line(&mut guess).expect("failed to read line");
+    println!("the secret number is {number}");
 
-    println!("the guessd number is {guess}")
+   
+    loop{
+        println!("Please input your guess.");
+        
+        
+        let mut guess = String::new();
+        
+        io::stdin().read_line(&mut guess).expect("failed to read line");
+        
+        println!("the guessd number is {guess}");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+
+        match guess.cmp(&number){
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            } 
+                
+          }
+
+    }
+    
 
 
 }
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/yeab/Rust/guessing_game/target/debug/guessing_game: /home/yeab/Rust/guessing_game/src/main.rs
